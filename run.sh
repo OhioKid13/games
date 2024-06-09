7a513ee7be53ef3675ca91a350bc4f1a9234e3a9
OhioKid13 <154306092+OhioKid13@users.noreply.github.com>
2024-06-09 10:37:46 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,6 @@
+REM IDK DO NOTHIng wiht this script
 clear
 pwsh main.ps1
+REM this script executes for powershell
+Set-ExecutionPolicy RemoteSigned & REM Click yes if prompted
+REM When you open powershell console use the cd command to go to the directory with the files and typ .\main.ps1
