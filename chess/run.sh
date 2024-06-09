908a41830e53621e1415ff36e5ff32a9ed0a08ed
OhioKid13 <154306092+OhioKid13@users.noreply.github.com>
2024-06-09 10:39:23 -0400

    Update run.sh

--- chess/run.sh
+++ chess/run.sh
@@ -1,6 +1,6 @@
-# IDK DO NOTHIng wiht this script
+REM IDK DO NOTHIng wiht this script
 clear
 pwsh main.ps1
-# this script executes for powershell
-Set-ExecutionPolicy RemoteSigned # Click yes if prompted
-# When you open powershell console use the cd command to go to the directory with the files and typ .\main.ps1
+REM this script executes for powershell
+Set-ExecutionPolicy RemoteSigned & REM Click yes if prompted
+REM When you open powershell console use the cd command to go to the directory with the files and typ .\main.ps1
